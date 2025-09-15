6752c1020b439aab847b999effdea349af828282
winverse <public.winverse@gmail.com>
2025-09-15 13:30:22 +0900

    feat: remove contact 파일

--- 01-basic-routing/src/main.jsx
+++ 01-basic-routing/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-
 import './styles/reset.css';
 import './index.css';
 
