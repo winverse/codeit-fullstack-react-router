f78ce142f29c914bd4f7e1e372d1c90bad8cd365
winverse <public.winverse@gmail.com>
2025-09-16 14:30:04 +0900

    feat: update 07

--- 07-dynamic-head/src/main.jsx
+++ 07-dynamic-head/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-// TODO: HelmetProvider를 import 하세요.
 
 import './styles/reset.css';
 import './index.css';
@@ -12,7 +11,6 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
-    {/* TODO: HelmetProvider로 BrowserRouter를 감싸세요. */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
